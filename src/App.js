bbe9c3ff5da02fc0da46c0044db971a8002fd6bd
anthonyBgood <anthonyBpostBox@gmail.com>
2019-03-15 13:11:12 +0000

    finished

--- src/App.js
+++ src/App.js
@@ -106,6 +106,19 @@ class App extends Component {
           <h1 className="App-title">ReactND - Coding Practice</h1>
         </header>
         <h2>Favorite Movies</h2>
+		<ul>
+			{profiles.map(profile => {
+             const userName = users[profile.userID].name;
+			 const favMovieName = movies[profile.favoriteMovieID].name 
+
+			return (
+            		<li key ={profile.id}>
+						<p>{userName}'s favorite movie is "{favMovieName}".</p>
+							{/*<p>{`${userName}'s favorite movie is "${favMovieName}."`}</p>*/}
+					</li>
+            );
+            })}
+		</ul>
       </div>
     );
   }
